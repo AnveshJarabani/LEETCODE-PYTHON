d13d5fcfef84efc923c96b14e0204356bd0ba62a
anveshjarabani <anveshjarabani@gmail.com>
2023-05-01 00:06:15 -0700

    leetcode 5.1.23

--- 111.mindepth.py
+++ 111.mindepth.py
@@ -1,15 +1,16 @@
-
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        def height(self,root,cur_depth=0):
-            if not root:
-                return 
-            if root.left and root.right:
-                self.min_depth=min(self.min_depth,cur_depth+1)
-            self.height(root.left,cur_depth+1)
-            self.height(root.right,cur_depth+1)
         if not root:
             return 0
-        self.min_depth=float('inf')
-        self.height(root)
-        return self.min_depth
+        que=[]
+        d=1
+        que.append((root,d))
+        while que:
+            cur,d=que.pop(0)
+            if not cur.left and not cur.right:
+                return d
+            if cur.left:
+                que.append((cur.left,d+1))
+            if cur.right:
+                que.append((cur.right,d+1))
+        return d
