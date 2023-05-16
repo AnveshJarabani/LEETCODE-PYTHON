aa3f5bde3f85e3b4a78bcadbff527cdc98726b4b
Anvesh Jarabani <AJarabani@uct.com>
2023-05-16 16:39:51 -0700

    283 move_zeros

--- 283move_zeros.py
+++ 283move_zeros.py
@@ -1,11 +1,14 @@
 import ast 
 in_lst=ast.literal_eval(input())
 def move_zeroes(in_lst):
-    for i in in_lst:
-        if i==0:
-            in_lst.remove(0)
-            in_lst.append(0)
-    return in_lst
-
-
+    # for i in in_lst:
+    #     if i==0:
+    #         in_lst.remove(0)
+    #         in_lst.append(0)
+    # return in_lst
+    l=0
+    for r in range(len(in_lst)):
+        if in_lst[r]:
+            in_lst[l],in_lst[r]=in_lst[r],in_lst[l]
+            l+=1
 print(move_zeroes(in_lst))
