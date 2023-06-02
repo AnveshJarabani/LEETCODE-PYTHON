ec77cdd16ad046a7e663dd34551c376899d869d4
anveshjarabani <anveshjarabani@gmail.com>
2023-06-02 15:10:35 -0700

    updates

--- udem.py
+++ udem.py
@@ -1,7 +1,7 @@
 import pyautogui as sim
 import time
-for i in range(1,5):
-    sim.click(x=2776, y=1483) # video position
+for i in range(1,4):
+    sim.click(x=2776, y=1485) # video position
     time.sleep(3)
-    sim.click(x=3028, y=358)  # next vid
+    sim.click(x=2845, y=851)  # next vid
     time.sleep(3)
