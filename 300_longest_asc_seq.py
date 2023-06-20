de7275756cae8d29d3bad2e96d249e133f339aae
anveshjarabani <anveshjarabani@gmail.com>
2023-06-19 18:33:26 -0700

    updates

--- 300_longest_asc_seq.py
+++ 300_longest_asc_seq.py
@@ -1,14 +1,15 @@
 import ast
-input=ast.literal_eval(input())
-def lenghtoflis(nums):
-    hash_list=[1]*len(nums)
-    for i in range(len(nums),-1,-1):
-        for j in range(i+1,len(nums)):
-            if nums[i]<nums[j]:
-                hash_list[i]=max(hash_list[i],hash_list[j]+1)
-    return max(hash_list)
-print(lenghtoflis(input))
 
-        
-        
-        
+input = ast.literal_eval(input())
+
+
+def lenghtoflis(nums):
+    hash_list = [1] * len(nums)
+    for i in range(len(nums), -1, -1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] < nums[j]:
+                hash_list[i] = max(hash_list[i], hash_list[j] + 1)
+    return max(hash_list)
+
+
+print(lenghtoflis(input))
