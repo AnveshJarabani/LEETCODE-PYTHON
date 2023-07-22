fa25aea967d59ee2b3adcdb82df52687b0d1782e
anveshjarabani <anveshjarabani@gmail.com>
2023-07-22 11:30:42 -0700

    updates

--- 3_longsubstring.py
+++ 3_longsubstring.py
@@ -1,13 +1,18 @@
+""" 
+This is a classic sliding window problem. 
+When we need to find "longest" something, try to use sliding window. 
+you start with no set and then keep updating the set as you traverse a given string/list
+"""
+
+
 class Solution:
-    def lls(self,s):
-        l,res=0,0
-        char_set=set()
-        for i in range(len(s)):
-            while s[i] in char_set:
-                char_set.remove(s[l])
-                l+=1
-            char_set.add(s[i])
-            res=max(res,i-l+1)
-        return res
-print(Solution().lls(input()))  
-        
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        lp, result = 0, 0
+        char_set = set()
+        for rp in range(len(s)):
+            while s[rp] in char_set:
+                char_set.remove(s[lp])
+                lp += 1
+            char_set.add(s[rp])
+            result = max(result, rp - lp + 1)
+        return result
