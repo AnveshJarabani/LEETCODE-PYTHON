5e5f012b121862571986b98fa9074a9d8f121f5f
anveshjarabani <anveshjarabani@gmail.com>
2023-08-10 10:53:13 -0700

    updates

--- node_viewer.py
+++ node_viewer.py
@@ -1,6 +1,6 @@
 from pyvis import network
-
-network= network.Network(height=1000,width=1000,notebook=True,directed=True)
+network= network.Network(height=500,width=1000,notebook=True,directed=True)
 network.add_nodes(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'])
 network. add_edges([ ('A','B'),('A','C'),('B','D'),('B','E'),('C','F'),('C','G'),('F','H'),('F','I')])
+network.show_buttons(filter_=['physics'])
 network. show('test.html')
