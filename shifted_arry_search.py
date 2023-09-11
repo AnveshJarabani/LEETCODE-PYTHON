86e817ee051332c936ba882886ffae13a443b7b8
anveshjarabani <anveshjarabani@gmail.com>
2023-09-11 10:13:16 -0700

    updates

--- shifted_arry_search.py
+++ shifted_arry_search.py
@@ -27,29 +27,40 @@ shiftArr = [1,2]
 num = 2
 print(shifted_arr_search(shiftArr, num))
 """
-1,2,3,4,5 -> 3,4,5,1,2
-          
-input:  shiftArr = [9, 12, 17, 2, 4, 5], num = 2 # shiftArr is the
-                                                 # outcome of shifting
-                                                 # [2, 4, 5, 9, 12, 17]
-                                                 # three times to the left
-shiftAarr.index(num) o(n) o(1)
-1. offset of a [sorted distinct array] & [it's shifted by x positions]
-
-1,2,3,4,5
-
-9 12 1 2 4 5 6 7 8
-l 9, r 8, mid 
+Define the pivot index as representing the smallest element in nums.
 
 
-[9, 10, 12, 17, 2, 4, 5] - > 
-1. find the offset.
-2. use l,r at the offset point and offset-1
-3. continue with log(n) soution like a sorted arry. ->
-k=4
-l,r=4-4,3-4
-while 
+In a rotated sorted array, the pivot value signifies where the rotation occurs. It partitions the array (of length nnn) into two sorted portions nums[0 ~ pivot - 1] and nums[pivot ~ n - 1].
 
-1,r 
-output: 3 # since it’s the index of 2 in arr
+Approach 1: Find Pivot Index + Binary Search
+Intuition
+If you are not familiar with binary search, please refer to our explore cards Binary Search Explore Card. We will focus on the usage in this article and not the underlying principles or implementation details.
+
+To pinpoint the pivot value, we can employ a modified binary search algorithm and find the leftmost element that is smaller than or equal to the last element in nums.
+
+
+After identifying the middle element in the searching space [left ~ right], we compare nums[mid] with nums[-1].
+
+If nums[mid] > nums[-1], it suggests that the pivot value lies on the right of nums[mid]. We will then proceed with the right half of the search space, which is [mid + 1 ~ right].
+Otherwise, the pivot value is nums[mid] or it's situated to the left of nums[mid], we continue with the left half of the searching space, which is [left ~ mid - 1].
+
+By determining the pivot value, we set the boundaries for our subsequent binary searches. Once we have the pivot value, we can execute two binary searches on each half of the array to locate the target element.
+
+
+Note: the typical way to calculate mid is (left + right) / 2. However, a safer way is left + (right - left) / 2. The two equations are equivalent, but the second one is safer because it guarantees no number larger than right is ever stored. In the first equation, if left + right is huge, then it could end up overflowing.
+
+Algorithm
+Perform a binary search to locate the pivot element by initializing the boundaries of the searching space as left = 0 and right = n - 1. While left < right:
+
+Let mid = left + (right - left) // 2.
+If nums[mid] > nums[n - 1], this suggests that pivot is located to the right of mid, hence we set left = mid + 1. Otherwise, pivot could be either at mid or to the left of mid, in which case we should set right = mid - 1.
+Upon completion of the binary search, we have the pivot index denoted as pivot = left.
+
+nums consists of two sorted subarrays, nums[0 ~ left - 1] and nums[left ~ n - 1].
+
+Perform a binary search over nums[0 ~ left - 1] for target. If target is within this subarray, return its index.
+
+Otherwise, perform a binary search over nums[left ~ n - 1] for target. If target is within this subarray, return its index. Otherwise, return -1.
+
+Implementation
 """
