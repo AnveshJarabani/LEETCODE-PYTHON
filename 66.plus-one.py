c6f33c957a4d6d0ad855e607f00dc3212e58af99
Sathvika Gullapally <anveshjarabani@gmail.com>
2023-10-29 17:13:16 -0500

    updates

---
+++ 66.plus-one.py
@@ -0,0 +1,11 @@
+#
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num = str(int("".join([str(i) for i in digits])) + 1)
+        return [int(i) for i in num]
+
+
+print(Solution().plusOne(digits=[1, 2, 3]))
