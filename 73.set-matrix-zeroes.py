86cb067202913c53cbf453cd8139891f29a07377
Sathvika Gullapally <anveshjarabani@gmail.com>
2023-10-29 17:31:26 -0500

    updates

--- 73.set-matrix-zeroes.py
+++ 73.set-matrix-zeroes.py
@@ -1,15 +1,41 @@
-#
-# @lc app=leetcode id=73 lang=python3
-#
-# [73] Set Matrix Zeroes
-#
+from typing import List
 
-# @lc code=start
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        
-# @lc code=end
+        row_count, col_count = len(matrix), len(matrix[0])
+        zeros = []
+        # for i in range(row_count):
+        #     for j in range(col_count):
+        #         if matrix[i][j] == 0:
+        zeros = [
+            (i, j)
+            for i in range(row_count)
+            for j in range(col_count)
+            if matrix[i][j] == 0
+        ]
+        for i, j in zeros:
+            matrix[i] = [0] * col_count
+            for i in range(row_count):
+                matrix[i][j] = 0
+        return matrix
 
+
+matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+print(Solution().setZeroes(matrix))
+
+
+"""
+for i in row:
+    for j in i:
+    if matrix[i][j] == 0:
+        matrix[i]=[0]*n
+        for j in matrix[i]:
+            matrix[i][j]==0
+    zero_positions=[(i,j)..]
+for i,j in zero_positions:
+    matrix[i]=[0]*n
+    for i in range(rows):
+        matrix[i][j]=0
+    return matrix
+"""
